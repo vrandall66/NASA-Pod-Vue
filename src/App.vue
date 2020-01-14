bb2549295970767a3d17d6bb4415a20ec72e5c43
Vanessa Randall <vanessalrandall@gmail.com>
2020-01-13 18:49:29 -0700

    Render photo of the day

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,39 @@
 <template>
   <div id="app">
     <NavBar />
+    <PhotoDetail v-bind:current="current" />
   </div>
 </template>
 
 <script>
+import 'es6-promise/auto';
 import NavBar from './components/NavBar.vue';
+import PhotoDetail from './components/PhotoDetail.vue';
+import { getTodaysPhoto } from './utils/apiCalls/apiCalls.js';
 
 export default {
   name: 'app',
   components: {
-    NavBar
+    NavBar,
+    PhotoDetail
+  },
+  data() {
+    return {
+      current: {}
+    };
+  },
+  methods: {
+    async setTodayPhoto() {
+      try {
+        let photo = await getTodaysPhoto();
+        this.current = photo;
+      } catch (error) {
+        null;
+      }
+    }
+  },
+  mounted() {
+    this.setTodayPhoto();
   }
 };
 </script>
@@ -22,6 +45,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
