c30b6638e89de5ba9d8b3c33073ed7631bb85d47
Vanessa Randall <vanessalrandall@gmail.com>
2020-01-13 17:48:23 -0700

    Begin to build out helper function to get current date and change format for fetch call

---
+++ src/components/NavBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Title</h1>
+    <button>Today's Photo</button>
+    <button>This Month's Photos</button>
+  </div>
+</template>
+
+<script>
+// import date from "date-and-time";
+// import formatTodaysDate from "../utils/helpers/helpers.js";
+export default {
+  components: {},
+  data: function() {
+    return {
+      date: null
+    };
+  }
+};
+</script>
+
+<style></style>
