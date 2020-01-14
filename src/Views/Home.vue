cd8ad7cfe02c8183db8074343f3f52ac2bf0bb57
Vanessa Randall <vanessalrandall@gmail.com>
2020-01-14 16:32:12 -0700

    Render different 'views' on routes, not quite rendering all content yet

---
+++ src/Views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Photo of the Day</h1>
+    <MonthContainer />
+  </div>
+</template>
+
+<script>
+import MonthContainer from '../components/MonthContainer.vue';
+export default {};
+</script>
---
+++ src/Views/PhotosOfTheMonth.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Photos of the Month</h1>
+    <MonthContainer />
+  </div>
+</template>
+
+<script>
+import MonthContainer from '../components/MonthContainer.vue';
+export default {};
+</script>
