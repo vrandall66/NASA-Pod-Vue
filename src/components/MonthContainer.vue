224e37ec825e08217e4b2cfe0fca604ee428e337
Vanessa Randall <vanessalrandall@gmail.com>
2020-01-13 19:39:21 -0700

    Render monthly photos with hard coded date

---
+++ src/components/MonthContainer.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div v-for="photo in photos" v-bind:key="photo.date">
+      <PhotoCard v-bind:photo="photo" />
+    </div>
+  </div>
+</template>
+
+<script>
+import PhotoCard from './PhotoCard.vue';
+export default {
+  name: 'MonthContainer',
+  components: {
+    PhotoCard
+  },
+  props: ['photos']
+};
+</script>
+
+<style scoped></style>
--- src/components/PhotoDetail.vue
+++ src/components/PhotoDetail.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <img :src="current.url" />
+    <h2>{{ current.title }}</h2>
+    <h4>{{ this.date }}</h4>
+    <p>{{ current.explanation }}</p>
   </div>
 </template>
 
@@ -9,7 +12,21 @@ import 'es6-promise/auto';
 
 export default {
   name: 'PhotoDetail',
-  props: ['current']
+  props: ['current'],
+  data() {
+    return {
+      date: ''
+    };
+  },
+  methods: {
+    formatDate() {
+      let formatted = current.date.toLocaleDateString();
+      this.date = formatted;
+    }
+  },
+  mounted() {
+    this.formatDate();
+  }
 };
 </script>
 
